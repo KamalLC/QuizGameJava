fe6b8af0ca22d0375d860b01745e9a6c3e1e9273
Kamal Lamichhane <klamichhane08@gmail.com>
2022-10-30 14:36:23 +0545

    questionpack customized

--- Over/src/QuestionPack/Question.java
+++ Over/src/QuestionPack/Question.java
@@ -23,9 +23,9 @@ public class Question implements ActionListener {
     Statement stm;
     ResultSet rst;
     JLabel sn, email, name;
-    JLabel lbl, admin_lbl,toplbl, questionLbl, option1Lbl, option2Lbl, option3Lbl, option4Lbl, cor_ansLbl, levelLbl;
+    JLabel lbl, admin_lbl,toplbl, questionLbl, option1Lbl, option2Lbl, option3Lbl, option4Lbl, cor_ansLbl, levelLbl, submit, back;
     JTextField question, option1, option2, option3, option4, cor_ans, level;
-    JButton submit, back, back_to_admin, accept, reject, admin_request;
+    JButton back_to_admin, accept, reject, admin_request;
     String[] levelOptions = {"1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12"};
     int[] levelOptionsInt = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12};
     JComboBox<String> levelComboBox = new JComboBox<>(levelOptions);
@@ -66,9 +66,19 @@ public class Question implements ActionListener {
         toplbl.setVerticalAlignment(SwingConstants.CENTER);
         lbl.add(toplbl);
 
+        // levelLbl = new JLabel("Level: ");
+        // levelLbl.setOpaque(true);
+        // levelLbl.setBounds(150, 120, 50+80, 40);
+        // levelLbl.setForeground(Color.WHITE);
+        // levelLbl.setFont(new Font("MOnospace", Font.PLAIN, 15));
+        // levelLbl.setHorizontalAlignment(SwingConstants.CENTER);
+        // levelLbl.setVerticalAlignment(SwingConstants.CENTER);
+        // levelLbl.setBackground(new Color(205,0,0) );
+        // lbl.add(levelLbl);
+
         levelLbl = new JLabel("Level: ");
         levelLbl.setOpaque(true);
-        levelLbl.setBounds(150, 120, 50+80, 40);
+        levelLbl.setBounds(250, 120, 160, 40);
         levelLbl.setForeground(Color.WHITE);
         levelLbl.setFont(new Font("MOnospace", Font.PLAIN, 15));
         levelLbl.setHorizontalAlignment(SwingConstants.CENTER);
@@ -76,14 +86,29 @@ public class Question implements ActionListener {
         levelLbl.setBackground(new Color(205,0,0) );
         lbl.add(levelLbl);
 
-        levelComboBox.setBounds(340, 120, 80, 40);
+        // levelComboBox.setBounds(340, 120, 80, 40);
+        // levelComboBox.setBackground( new Color(204, 204, 204) );
+
+        // lbl.add(levelComboBox);
+        levelComboBox.setBounds(480, 120, 80, 40);
         levelComboBox.setBackground( new Color(204, 204, 204) );
 
         lbl.add(levelComboBox);
 
+        // questionLbl = new JLabel("Question: ");
+        // questionLbl.setOpaque(true);
+        // questionLbl.setBounds(100, 150, 50+40, 30);
+        // questionLbl.setForeground(Color.WHITE);
+        // questionLbl.setFont(new Font("MOnospace", Font.PLAIN, 15));
+        // questionLbl.setHorizontalAlignment(SwingConstants.CENTER);
+        // questionLbl.setVerticalAlignment(SwingConstants.CENTER);
+        // questionLbl.setBackground(new Color(205,0,0) );
+
+        // lbl.add(questionLbl);
+
         questionLbl = new JLabel("Question: ");
         questionLbl.setOpaque(true);
-        questionLbl.setBounds(100, 150, 50+40, 30);
+        questionLbl.setBounds(250, 190, 160, 40);
         questionLbl.setForeground(Color.WHITE);
         questionLbl.setFont(new Font("MOnospace", Font.PLAIN, 15));
         questionLbl.setHorizontalAlignment(SwingConstants.CENTER);
@@ -92,14 +117,29 @@ public class Question implements ActionListener {
 
         lbl.add(questionLbl);
 
+        // question = new JTextField();
+        // question.setBounds(200, 150, 400, 30);
+        // question.setBackground( new Color(204, 204, 204) );
+        // lbl.add(question);
+
+        // option1Lbl = new JLabel("Option1: ");
+        // option1Lbl.setOpaque(true);
+        // option1Lbl.setBounds(100, 200, 50+40, 30);
+        // option1Lbl.setForeground(Color.WHITE);
+        // option1Lbl.setFont(new Font("MOnospace", Font.PLAIN, 15));
+        // option1Lbl.setHorizontalAlignment(SwingConstants.CENTER);
+        // option1Lbl.setVerticalAlignment(SwingConstants.CENTER);
+        // option1Lbl.setBackground(new Color(205,0,0) );
+        // lbl.add(option1Lbl);
+
         question = new JTextField();
-        question.setBounds(200, 150, 400, 30);
+        question.setBounds(480, 190, 500, 40);
         question.setBackground( new Color(204, 204, 204) );
         lbl.add(question);
 
         option1Lbl = new JLabel("Option1: ");
         option1Lbl.setOpaque(true);
-        option1Lbl.setBounds(100, 200, 50+40, 30);
+        option1Lbl.setBounds(250, 260, 160, 40);
         option1Lbl.setForeground(Color.WHITE);
         option1Lbl.setFont(new Font("MOnospace", Font.PLAIN, 15));
         option1Lbl.setHorizontalAlignment(SwingConstants.CENTER);
@@ -107,14 +147,29 @@ public class Question implements ActionListener {
         option1Lbl.setBackground(new Color(205,0,0) );
         lbl.add(option1Lbl);
 
+        // option1 = new JTextField();
+        // option1.setBounds(200, 200, 400, 30);
+        // option1.setBackground( new Color(204, 204, 204) );
+        // lbl.add(option1);
+
         option1 = new JTextField();
-        option1.setBounds(200, 200, 400, 30);
+        option1.setBounds(480, 260, 500, 40);
         option1.setBackground( new Color(204, 204, 204) );
         lbl.add(option1);
 
+        // option2Lbl = new JLabel("Option2: ");
+        // option2Lbl.setOpaque(true);
+        // option2Lbl.setBounds(100, 250, 50+40, 30);
+        // option2Lbl.setForeground(Color.WHITE);
+        // option2Lbl.setFont(new Font("MOnospace", Font.PLAIN, 15));
+        // option2Lbl.setHorizontalAlignment(SwingConstants.CENTER);
+        // option2Lbl.setVerticalAlignment(SwingConstants.CENTER);
+        // option2Lbl.setBackground(new Color(205,0,0) );
+        // lbl.add(option2Lbl);
+
         option2Lbl = new JLabel("Option2: ");
         option2Lbl.setOpaque(true);
-        option2Lbl.setBounds(100, 250, 50+40, 30);
+        option2Lbl.setBounds(250, 330, 160, 40);
         option2Lbl.setForeground(Color.WHITE);
         option2Lbl.setFont(new Font("MOnospace", Font.PLAIN, 15));
         option2Lbl.setHorizontalAlignment(SwingConstants.CENTER);
@@ -122,14 +177,29 @@ public class Question implements ActionListener {
         option2Lbl.setBackground(new Color(205,0,0) );
         lbl.add(option2Lbl);
 
+        // option2 = new JTextField();
+        // option2.setBounds(200, 250, 400, 30);
+        // option2.setBackground( new Color(204, 204, 204) );
+        // lbl.add(option2);
+
         option2 = new JTextField();
-        option2.setBounds(200, 250, 400, 30);
+        option2.setBounds(480, 330, 500, 40);
         option2.setBackground( new Color(204, 204, 204) );
         lbl.add(option2);
 
         option3Lbl = new JLabel("Option3: ");
+        // option3Lbl.setOpaque(true);
+        // option3Lbl.setBounds(100, 300, 50+40, 30);
+        // option3Lbl.setForeground(Color.WHITE);
+        // option3Lbl.setFont(new Font("MOnospace", Font.PLAIN, 15));
+        // option3Lbl.setHorizontalAlignment(SwingConstants.CENTER);
+        // option3Lbl.setVerticalAlignment(SwingConstants.CENTER);
+        // option3Lbl.setBackground(new Color(205,0,0) );
+        // lbl.add(option3Lbl);
+
+        option3Lbl = new JLabel("Option3: ");
         option3Lbl.setOpaque(true);
-        option3Lbl.setBounds(100, 300, 50+40, 30);
+        option3Lbl.setBounds(250, 400, 160, 40);
         option3Lbl.setForeground(Color.WHITE);
         option3Lbl.setFont(new Font("MOnospace", Font.PLAIN, 15));
         option3Lbl.setHorizontalAlignment(SwingConstants.CENTER);
@@ -137,14 +207,29 @@ public class Question implements ActionListener {
         option3Lbl.setBackground(new Color(205,0,0) );
         lbl.add(option3Lbl);
 
+        // option3 = new JTextField();
+        // option3.setBounds(200, 300, 400, 30);
+        // option3.setBackground( new Color(204, 204, 204) );
+        // lbl.add(option3);
+
+        // option4Lbl = new JLabel("Option4: ");
+        // option4Lbl.setOpaque(true);
+        // option4Lbl.setBounds(100, 350, 50+40, 30);
+        // option4Lbl.setForeground(Color.WHITE);
+        // option4Lbl.setFont(new Font("MOnospace", Font.PLAIN, 15));
+        // option4Lbl.setHorizontalAlignment(SwingConstants.CENTER);
+        // option4Lbl.setVerticalAlignment(SwingConstants.CENTER);
+        // option4Lbl.setBackground(new Color(205,0,0) );
+        // lbl.add(option4Lbl);
+
         option3 = new JTextField();
-        option3.setBounds(200, 300, 400, 30);
+        option3.setBounds(480, 400, 500, 40);
         option3.setBackground( new Color(204, 204, 204) );
         lbl.add(option3);
 
         option4Lbl = new JLabel("Option4: ");
         option4Lbl.setOpaque(true);
-        option4Lbl.setBounds(100, 350, 50+40, 30);
+        option4Lbl.setBounds(250, 470, 160, 40);
         option4Lbl.setForeground(Color.WHITE);
         option4Lbl.setFont(new Font("MOnospace", Font.PLAIN, 15));
         option4Lbl.setHorizontalAlignment(SwingConstants.CENTER);
@@ -152,14 +237,29 @@ public class Question implements ActionListener {
         option4Lbl.setBackground(new Color(205,0,0) );
         lbl.add(option4Lbl);
 
+        // option4 = new JTextField();
+        // option4.setBounds(200, 350, 400, 30);
+        // option4.setBackground( new Color(204, 204, 204) );
+        // lbl.add(option4);
+
+        // cor_ansLbl = new JLabel("Right Ans: ");
+        // cor_ansLbl.setOpaque(true);
+        // cor_ansLbl.setBounds(100, 400, 50+40, 30);
+        // cor_ansLbl.setForeground(Color.WHITE);
+        // cor_ansLbl.setFont(new Font("MOnospace", Font.PLAIN, 15));
+        // cor_ansLbl.setHorizontalAlignment(SwingConstants.CENTER);
+        // cor_ansLbl.setVerticalAlignment(SwingConstants.CENTER);
+        // cor_ansLbl.setBackground(new Color(205,0,0) );
+        // lbl.add(cor_ansLbl);
+
         option4 = new JTextField();
-        option4.setBounds(200, 350, 400, 30);
+        option4.setBounds(480, 470, 500, 40);
         option4.setBackground( new Color(204, 204, 204) );
         lbl.add(option4);
 
         cor_ansLbl = new JLabel("Right Ans: ");
         cor_ansLbl.setOpaque(true);
-        cor_ansLbl.setBounds(100, 400, 50+40, 30);
+        cor_ansLbl.setBounds(250, 540, 160, 40);
         cor_ansLbl.setForeground(Color.WHITE);
         cor_ansLbl.setFont(new Font("MOnospace", Font.PLAIN, 15));
         cor_ansLbl.setHorizontalAlignment(SwingConstants.CENTER);
@@ -169,27 +269,120 @@ public class Question implements ActionListener {
 
 
         
-        optionComboBox.setBounds(200, 400, 100, 30);
+        // optionComboBox.setBounds(200, 400, 100, 30);
+        // optionComboBox.setBackground( new Color(204, 204, 204) );
+        // lbl.add(optionComboBox);
+
+        // submit = new JButton("Submit");
+        // submit.setBounds(150, 450, 150, 30);
+        // submit.setForeground(Color.white);
+        // submit.setBackground( new Color(205, 0, 0) );
+        // lbl.add(submit);
+        // submit.addActionListener(this);
+
+        optionComboBox.setBounds(480,540, 100, 40);
         optionComboBox.setBackground( new Color(204, 204, 204) );
         lbl.add(optionComboBox);
 
-        submit = new JButton("Submit");
+        submit = new JLabel("Submit");
         submit.setBounds(150, 450, 150, 30);
         submit.setForeground(Color.white);
         submit.setBackground( new Color(205, 0, 0) );
         lbl.add(submit);
-        submit.addActionListener(this);
+        
 
-        back = new JButton("Home");
-        back.setBounds(300, 550, 150, 30);
-        back.setForeground(Color.white);
-        back.setBackground( new Color(205, 0, 0) );
+        // back = new JButton("Home");
+        // back.setBounds(300, 550, 150, 30);
+        // back.setForeground(Color.white);
+        // back.setBackground( new Color(205, 0, 0) );
+        
+        // lbl.add(back);
+        // back.addActionListener(this);
+        back = new JLabel("< - - HomePage ");
+        back.setFont(new Font("MOnospace", Font.PLAIN, 18));
+        back.setForeground(new Color(205,0,0) );
+        back.setOpaque(false);
+        
+        back.setBounds(232, 600, 160, 40);
         
         lbl.add(back);
-        back.addActionListener(this);
+
+        submit = new JLabel("Submit - - >");
+        submit.setFont(new Font("MOnospace", Font.PLAIN, 18));
+        submit.setForeground(new Color(205,0,0) );
+        submit.setOpaque(false);
+        
+        submit.setBounds(1000, 600, 160, 40);
+        
+        lbl.add(submit);
+
+
+        back.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+
+        back.addMouseListener(new MouseAdapter() {
+          @Override
+          public void mouseClicked(MouseEvent e) {
+              try {
+                 
+                lbl.setVisible(false);
+                // this is for linking another file where should this label directs.
+                new FirstInterface(f, stm);
+               
+              } catch (Exception e1) {
+  
+                  e1.printStackTrace();
+              }
+          }
+      });
+
+      submit.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+
+      submit.addMouseListener(new MouseAdapter() {
+        @Override
+        public void mouseClicked(MouseEvent e) {
+            try {
+               
+                String qsn, op1, op2, op3, op4, corr;
+                int lev;
+    
+                lev = Integer.parseInt(levelComboBox.getItemAt(levelComboBox.getSelectedIndex()));
+    
+                qsn = question.getText();
+                op1 = option1.getText();
+                op2 = option2.getText();
+                op3 = option3.getText();
+                op4 = option4.getText();
+    
+                String[] optionList = {op1, op2, op3, op4};
+                corr = optionList[optionComboBox.getSelectedIndex()];
+                System.out.println(lev);
+    
+                
+                    // stm = conn.createStatement();
+                    System.out.println("testing");
+                    String query = "INSERT INTO question_list(question, option_a, option_b, option_c, option_d, correct_answer, level) VALUES('"+qsn+"','"+op1+"','"+op2+"','"+op3+"','"+op4+"','"+corr+"','"+lev+"')";
+                    System.out.println("testing 2");
+                    stm.executeUpdate(query);
+                    System.out.println("Inserted into table");
+                    question.setText("");
+                    option1.setText("");
+                    option2.setText("");
+                    option3.setText("");
+                    option4.setText("");
+                
+             
+            } catch (Exception e1) {
+
+                e1.printStackTrace();
+            }
+        }
+    });
+    
+      
+
 
         admin_request = new JButton("ADMIN REQUEST");
-        admin_request.setBounds(650, 250, 150, 30);
+        admin_request.setBounds(1120, 65, 150, 40);
         admin_request.setForeground(Color.white);
         admin_request.setBackground( new Color(205, 0, 0) );
         lbl.add(admin_request);
